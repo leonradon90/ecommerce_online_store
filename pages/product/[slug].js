02e793f9cf0d4347d974e28ef4219559628b8f93
leonradon90 <52747023+leonradon90@users.noreply.github.com>
2023-01-12 16:28:50 +0100

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -21,7 +21,7 @@ const ProductDetails = ({ product, products }) => {
         <div className="product-detail-container">
             <div>
                 <div className="image-container">
-                    <img src={urlFor(image && image [index])} className="product-detail-image"/>
+                    <img src={urlFor(image && image [index])} className="product-detail-image" alt=''/>
 
                 </div>
                 <div className="small-images-container">
